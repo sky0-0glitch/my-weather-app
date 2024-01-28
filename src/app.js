be2b3b492fbfc86a0b22a9a48657c4f17dcb9ec5
sky0-0glitch <157227184+sky0-0glitch@users.noreply.github.com>
2024-01-28 14:17:51 +0500

    added real data

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+cityweather("Kabul");
 let formjs = document.querySelector("#form");
 formjs.addEventListener("submit", replacingFunction);
 function replacingFunction(event) {
@@ -12,10 +13,39 @@ function cityweather(city) {
   axios.get(apiurl).then(refresh);
 }
 function refresh(response) {
+  console.log(response.data);
+  let currenttime = document.querySelector("#time");
+  let currentdescription = document.querySelector("#description");
+  let currenthumadity = document.querySelector("#humadity");
+  let currenticon = document.querySelector("#icon");
+  let currentwind = document.querySelector("#wind");
+  let date = new Date(response.data.time * 1000);
   let oldDegree = document.querySelector("#current");
   let newdegree = response.data.temperature.current;
   oldDegree.innerHTML = Math.round(newdegree);
   let header = document.querySelector("#head");
   header.innerHTML = response.data.city;
+  currenttime.innerHTML = formatedate(date);
+  currentdescription.innerHTML = response.data.condition.description;
+  currenthumadity.innerHTML = `${response.data.temperature.humidity}%`;
+  currentwind.innerHTML = `${response.data.wind.speed}km/h`;
+  currenticon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 }
-cityweather("Kabul");
+function formatedate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}${hours}:${minutes}`;
+}
