44407a92f89fbf2e3449b0efaa51e4f32041eef6
sky0-0glitch <157227184+sky0-0glitch@users.noreply.github.com>
2024-01-28 12:46:04 +0500

    added api

--- src/app.js
+++ src/app.js
@@ -3,6 +3,19 @@ formjs.addEventListener("submit", replacingFunction);
 function replacingFunction(event) {
   event.preventDefault();
   let cityjs = document.querySelector("#city");
-  let header = document.querySelector("#head");
-  header.innerHTML = cityjs.value;
+
+  cityweather(cityjs.value);
 }
+function cityweather(city) {
+  let apikey = "2710a7433443ato3f2a46b02f714f1fe";
+  let apiurl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+  axios.get(apiurl).then(refresh);
+}
+function refresh(response) {
+  let oldDegree = document.querySelector("#current");
+  let newdegree = response.data.temperature.current;
+  oldDegree.innerHTML = Math.round(newdegree);
+  let header = document.querySelector("#head");
+  header.innerHTML = response.data.city;
+}
+cityweather("Kabul");
